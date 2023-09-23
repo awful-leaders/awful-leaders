0e61d094611c8ebb728c33f0714a6bc8912f31a7
awful-leaders-admin <awful-leaders-admin@users.noreply.github.com>
2023-09-23 08:36:57 -0400

    Deploy at 924311d71b322be1079070180d66758b83f2b697

---
+++ _next/static/chunks/app/layout-1021e50e4ac20cc6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{8926:function(n,e,t){Promise.resolve().then(t.t.bind(t,5135,23)),Promise.resolve().then(t.t.bind(t,8437,23))},5135:function(){},8437:function(){}},function(n){n.O(0,[971,864,744],function(){return n(n.s=8926)}),_N_E=n.O()}]);
